925a35ed323d692de5fedf33bdaf8710e99caab3
justosky <justosky2011@yahoo.com>
2023-08-09 15:41:22 +0100

    implementing file storage class that will serialize python object and store in file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Importing the neccessary modules
+"""
+import json
+import os
+from datetime import datetime
+
+"""
+Defining FileStorage class
+"""
+class FileStorage:
+    pass
